aaf1869f30585ad7ae9a6b2dbe77b7496d55adec
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-11-08 13:50:04 +0600

    service data get function added

--- index.js
+++ index.js
@@ -11,14 +11,21 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mxrfp9v.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 async function run(){
     const serviceCollection = client.db('travelBee').collection('services');
     try {
-
-    } finally {
+        app.get('/services', async(req, res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    } 
+    finally {
 
     }
 }
