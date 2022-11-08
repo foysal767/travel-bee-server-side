6dfa3c6f08d2e026a86f6afc782da4ab4dd9dcf2
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-11-08 23:49:09 +0600

    details data loaded function added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,6 +24,21 @@ async function run() {
             const cursor = serviceCollection.find(query).limit(3)
             const services = await cursor.toArray();
             res.send(services)
+        })
+
+        app.get('/all-service', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+
+        app.get('/all-service/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
         })
     }
     finally {
