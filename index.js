bea755e60b71059971050cb7b0f787e970688efb
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-11-08 21:09:48 +0600

    env setup problem solved

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,33 +10,33 @@ app.use(cors());
 app.use(express.json());
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mxrfp9v.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-async function run(){
-    const serviceCollection = client.db('travelBee').collection('services');
+async function run() {
     try {
-        app.get('/services', async(req, res) =>{
+        const serviceCollection = client.db('travelBee').collection('services');
+
+        app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
             const services = await cursor.toArray();
             res.send(services)
         })
-    } 
+    }
     finally {
 
     }
 }
-run().catch(err => console.log(err));
+run().catch(err => console.error(err));
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Travel bee server is running!')
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Travel bee server running on ${port}`)
 })
